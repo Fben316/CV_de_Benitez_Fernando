9eea6b541d4dc2b51566817080795f515ebea8e5
Fben316 <110858226+Fben316@users.noreply.github.com>
2023-02-25 15:56:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function ocultar(){
+    document.getElementById('hideText').style.display = 'none';
+    document.getElementById('hideText2').style.display = 'none';
+    document.getElementById('hideText3').style.display = 'none';
+    document.getElementById('hideText4').style.display = 'none';
+    document.getElementById('hideText5').style.display = 'none';
+    document.getElementById('hideText6').style.display = 'none';
+    document.getElementById('boton1').style.display = 'block';
+    document.getElementById('boton2').style.display = 'none';
+    
+}
+
+function mostrar(){
+    document.getElementById('hideText').style.display = 'block';
+    document.getElementById('hideText2').style.display = 'block';
+    document.getElementById('hideText3').style.display = 'block';
+    document.getElementById('hideText4').style.display = 'block';
+    document.getElementById('hideText5').style.display = 'block';
+    document.getElementById('hideText6').style.display = 'block';
+    document.getElementById('boton1').style.display = 'none';
+    document.getElementById('boton2').style.display = 'block';
+    
+}
